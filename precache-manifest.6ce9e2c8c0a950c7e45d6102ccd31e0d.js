4fbf5d994c8da38bb33515b808210b472a29f66c
GenaKashin <gengambit37@gmail.com>
2020-10-01 13:39:30 +0300

    Added vue.config.js

---
+++ precache-manifest.6ce9e2c8c0a950c7e45d6102ccd31e0d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b72829154a8523b847d",
+    "url": "/task_manager/css/app.7b6111ef.css"
+  },
+  {
+    "revision": "e2c6aa03d66ad90988b7",
+    "url": "/task_manager/css/chunk-cb13985e.a8f248b0.css"
+  },
+  {
+    "revision": "7069b88ec14b39708f1b",
+    "url": "/task_manager/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "3de91f4434837f46e1a92ac5b6fa8dc5",
+    "url": "/task_manager/index.html"
+  },
+  {
+    "revision": "3b72829154a8523b847d",
+    "url": "/task_manager/js/app.8fd0e7b0.js"
+  },
+  {
+    "revision": "0a9c1299d51137eb34c1",
+    "url": "/task_manager/js/chunk-2d0b64c8.c233208b.js"
+  },
+  {
+    "revision": "af19fb0e11a62c76b633",
+    "url": "/task_manager/js/chunk-2d21ef2c.44ce3501.js"
+  },
+  {
+    "revision": "e2c6aa03d66ad90988b7",
+    "url": "/task_manager/js/chunk-cb13985e.8e365e78.js"
+  },
+  {
+    "revision": "7069b88ec14b39708f1b",
+    "url": "/task_manager/js/chunk-vendors.a235fb75.js"
+  },
+  {
+    "revision": "9589a2e769485a9039be09a91e817ced",
+    "url": "/task_manager/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/task_manager/robots.txt"
+  }
+]);
